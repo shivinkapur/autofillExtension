f9fea4fbe75d606936d8fcb0fe3d8aa5b247b2e4
shivinkapur <shivinkapur@hotmail.com>
2016-02-28 09:37:05 -0800

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+jQuery(function($){
+  var Firebase = require("firebase");
+  var myFirebaseRef = new Firebase("https://cardmill.firebaseio.com/");
+  myFirebaseRef.set({
+    title: "Hello World!",
+    author: "Firebase",
+    location: {
+      city: "San Francisco",
+      state: "California",
+      zip: 94103
+    }
+  });
+});
